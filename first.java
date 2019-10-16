70e5e5b4a3d6fb0287bd7b08724ef156011a31ce
Chris <christopher_hogue@hotmail.com>
2019-10-15 22:55:51 -0700

    Update first.java

--- first.java
+++ first.java
@@ -2,6 +2,7 @@ public class First {
 	public static void main (String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hi");
+		System.out.println("Hi Diddly Ho there, Neighborino!");
 	}
 	
 	
